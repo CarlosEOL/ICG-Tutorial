72853f626a84a6f564a00cb4502339c469864fe1
Carlos. L <74547522+CarlosEOL@users.noreply.github.com>
2023-02-01 05:30:27 -0500

    Swapper

--- Assets/MaterialSwapper.cs
+++ Assets/MaterialSwapper.cs
@@ -1,18 +1,69 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MaterialSwapper : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<Material> Materials;
+    private Renderer Renderer;
+
+    private void Start()
     {
-        
+        Renderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            Renderer.sharedMaterial = Materials[0];
+        }
         
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            Renderer.sharedMaterial = Materials[1];
+        }
+        
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            Renderer.sharedMaterial = Materials[2];
+        }
+        
+        if (Input.GetKey(KeyCode.Alpha4))
+        {
+            Renderer.sharedMaterial = Materials[3];
+        }
+        
+        if (Input.GetKey(KeyCode.Alpha5))
+        {
+            Renderer.sharedMaterial = Materials[4];
+        }
+        
+        if (Input.GetKey(KeyCode.Alpha6))
+        {
+            Renderer.sharedMaterial = Materials[5];
+        }
+        
+        if (Input.GetKey(KeyCode.Alpha7))
+        {
+            Renderer.sharedMaterial = Materials[6];
+        }
+        
+        if (Input.GetKey(KeyCode.Alpha8))
+        {
+            Renderer.sharedMaterial = Materials[7];
+        }
+        
+        if (Input.GetKey(KeyCode.Alpha9))
+        {
+            Renderer.sharedMaterial = Materials[8];
+        }
+        
+        if (Input.GetKey(KeyCode.Alpha0))
+        {
+            Renderer.sharedMaterial = Materials[9];
+        }
     }
 }
